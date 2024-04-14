0feae4a4f0cee1678400e11d871574dd941ad1a4
Sonali Patnaik <sonalipatnaik1991@gmail.com>
2024-04-14 22:49:23 +0530

    collapsible side menu

--- src/components/LocalInteriors/LocalInteriors.js
+++ src/components/LocalInteriors/LocalInteriors.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './LocalInteriors.css';
 import InteriorDetailsCard from "./InteriorDetailsCard/InteriorDetailsCard";
 import Filters from "./Filters/Filters";
+
 const LocalInteriors = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
     const state = {
         designers: [
             { id: 1, name: 'John Doe', experience: '5 years', location: 'New York', rating: 4.5 },
@@ -14,21 +17,29 @@ const LocalInteriors = () => {
             { id: 2, name: 'Profession', values: ['Designer', 'Decorator'] },
             { id: 3, name: 'Material', values: ['PVC', 'Aluminium', 'Wood'] },
             { id: 4, name: 'Property Type', values: ['Residential', 'Commercial', 'Corporate'] },
-            { id: 5, name: 'Project Type', values: ['Modular Kitchen', 'Furniuture', 'Ceiling', 'Wall', 'Wardrobe', 'Bathrooms',] },
+            { id: 5, name: 'Project Type', values: ['Modular Kitchen', 'Furniture', 'Ceiling', 'Wall', 'Wardrobe', 'Bathrooms'] },
             { id: 6, name: 'Ratings', values: ['Any', '3.5', '4.0', '4.5', '5.0'] }
         ]
+    };
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
     };
 
     return (
         <div className='local-interiors-container'>
-            <div className="filter-container" >
-                <Filters filters={state.filters} />
+            <div className={`filter-container side-menu ${isOpen ? 'open' : ''}`}>
+                <div className="filter">
+                    <Filters filters={state.filters} />
+                </div>
             </div>
             <div className='details'>
-                <h1>Interior Designers</h1>
+                <div className='list-header'>
+                    <h4>Interior Designers</h4>
+                    <i className="bi bi-filter-circle" onClick={toggleMenu}></i>
+                </div>
                 {state.designers.map(designer => (
-                    <InteriorDetailsCard designer={designer} key={designer?.id}/>
+                    <InteriorDetailsCard designer={designer} key={designer?.id} />
                 ))}
             </div>
         </div>
